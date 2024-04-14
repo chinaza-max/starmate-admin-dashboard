ecf9cb4d6d4c71271ca7e0b025979ca36c491b43
chinaza-max <mosesogbonna68@gmail.com>
2024-04-14 19:26:47 +0100

    first commit

---
+++ src/app/hooks/useSettlement.js
@@ -0,0 +1,6 @@
+import { useContext } from 'react';
+import SettlementContext from 'app/contexts/SettlementContext';
+
+const Settlements = () => useContext(SettlementContext);
+
+export default Settlements;
